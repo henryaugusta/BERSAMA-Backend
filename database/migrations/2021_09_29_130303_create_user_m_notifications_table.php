804f4789022da3ae1da169d574cf0f7be15ad6b2
henryaugusta <henryaugusta4@gmail.com>
2021-10-15 10:38:38 +0700

    Finish CRUD Konten/News and delete unnecessary files

--- database/migrations/2021_09_29_130303_create_user_m_notifications_table.php
+++ database/migrations/2021_09_29_130303_create_user_m_notifications_table.php
@@ -16,14 +16,12 @@ class CreateUserMNotificationsTable extends Migration
         Schema::create('user_m_notifications', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->nullable();
-            $table->unsignedBigInteger('rs_id')->nullable();
             $table->string('title')->nullable();
             $table->text('desc')->nullable();   // for short desc
             $table->string('message')->nullable(); // for long text desc
             $table->string('type')->nullable();  // 1 for app notif, 2 for rs notif update.
             $table->boolean('is_read')->nullable()->default(false);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('rs_id')->references('id')->on('request_sells')->onDelete('cascade');
             $table->timestamps();
         });
     }
