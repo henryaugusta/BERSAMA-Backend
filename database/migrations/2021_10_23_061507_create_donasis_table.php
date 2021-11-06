eafe679be45fa21f753fd10aa1deffdc9b0afc31
henryaugusta <henryaugusta4@gmail.com>
2021-11-06 09:11:38 +0700

    donasi migration table

--- database/migrations/2021_10_23_061507_create_donasis_table.php
+++ database/migrations/2021_10_23_061507_create_donasis_table.php
@@ -15,6 +15,13 @@ class CreateDonasisTable extends Migration
     {
         Schema::create('donasis', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('accounts_id');
+            $table->string("show_as")->nullable();
+            $table->text("message")->nullable();
+            $table->string("photo_path")->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('accounts_id')->references('id')->on('donation_accounts')->onDelete('cascade');
             $table->timestamps();
         });
     }
