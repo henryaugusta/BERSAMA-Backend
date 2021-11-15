3e381c9c4ba502726418bb19e5c917aadc623d13
henryaugusta <henryaugusta4@gmail.com>
2021-11-15 23:36:55 +0700

    Refactor code --delete unnecessary files

--- app/Models/EatEventDocumentation.php
+++ app/Models/EatEventDocumentation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MappingRequestSellPhoto extends Model
+class EatEventDocumentation extends Model
 {
     use HasFactory;
 }
